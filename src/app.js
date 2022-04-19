b313280f9e1829e88d4339f98c4e777d98672fa6
cavem <kyle.stinar@hotmail.com>
2022-04-19 12:57:19 -0700

    connect knex and pg to the router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.use(express.json());
+app.use(cors());
+
+const decksRouter = require("./decks/decks-router");
+app.use("/decks", decksRouter);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,25 +1,8 @@
-// const app = require("./app");
-// const PORT = process.env.PORT || 5000;
+const app = require("./app");
 
-// app.listen(PORT, () => {
-//   console.log(`listening on port ${PORT}`);
-// });
-
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-const router = express.Router();
-const PORT = process.env.PORT || 5000;
-
-router.get("/", cors(), (req, res) => {
-  res.json({ message: "Hello Heroku!" });
-});
-
-app.use("/", router);
+require("dotenv").config();
+const { PORT = 5000 } = process.env;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT} `);
+  console.log(`Server running on ${PORT}`);
 });
-
-module.exports = app;
