e3ebef72119d49a336383f274d60b832e9812af0
cavem <kyle.stinar@hotmail.com>
2022-04-20 11:20:39 -0700

    hell 1

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,24 @@
 // Update with your config settings.
 require("dotenv").config();
+const parse = require("pg-connection-string").parse;
 const path = require("path");
 const { DATABASE_URL } = process.env;
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+const pgconfig = parse(DATABASE_URL);
+
+pgconfig.ssl = { rejectUnauthorized: false };
+
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: pgconfig,
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: { directory: path.join(__dirname, "src", "db", "seeds") },
   },
-  // production: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password",
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations",
-  //   },
-  // },
 };
