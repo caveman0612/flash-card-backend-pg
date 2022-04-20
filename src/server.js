5821848dea71b86c0f2f0390f59974173f17b6b2
cavem <kyle.stinar@hotmail.com>
2022-04-20 11:11:15 -0700

    hello

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 require("dotenv").config();
 // const { PORT = 5000 } = process.env;
-const PORT = process.env || "5000";
+const PORT = process.env.PORT || "5000";
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
